038428865cf5043209e467b5ffb992fafa42cdd0
Prasad Morkar <prasad.morkar@hertz.com>
2022-03-09 14:13:36 -0600

    updated code

--- src/index.js
+++ src/index.js
@@ -1,32 +1,63 @@
-//Calculate Tip
-function searchPokemon() {
-  var pokemon = document.getElementById("pokemon").value;
+var flag = true;
+const cachedPokemon = {};
+var currentPoke;
 
-  //validate input
-  if (pokemon === "") {
-    alert("Please enter values");
-    return;
-  }
-
-  fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(function (response) {
-      // The API call was successful!
-      console.log("success!", response.body);
-    })
-    .catch(function (err) {
-      // There was an error
-      console.warn("Something went wrong.", err);
-    });
-
-  document.getElementById("pokemonInfo").style.display = "block";
-  document.getElementById("info").style.display = "block";
-  document.getElementById("info").innerHTML = pokemon;
+async function getPokemonInfo(name) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const pokemon = await res.json();
+  currentPoke = pokemon;
+  console.log('Poke Data' + cachedPokemon[25]);
+  getPokemonGeneralInfo(pokemon);
 }
 
-//Hide the tip amount on load
-document.getElementById("pokemonInfo").style.display = "none";
-document.getElementById("info").style.display = "none";
-//click to call function
-document.getElementById("search").onclick = function () {
-  searchPokemon();
-};
+function getPokemonGeneralInfo(pokemon) {
+  $("#pokeInfo").text("Pokemon Name " + pokemon.name);
+  $("#pokemonImage").attr("src", pokemon.sprites.front_default);
+  $("#weight").text("Pokemon Weight  " + pokemon.weight);
+  $("#height").text("Pokemon Heigh " + pokemon.height);
+  $("#type1").text("Pokemon Type1 : " + pokemon.types[0].type.name);
+}
+
+$("#pokeButton").on("click", function () {
+  console.log("Button Clicked pokeButton");
+  value = $("#pokemonNumber").val();
+  getPokemonInfo(value);
+});
+
+$("#pokeSaveButton").on("click", function () {
+  var id = currentPoke["id"];
+  if (cachedPokemon[id] == null) {
+    console.log('Poke Data' + id);
+    cachedPokemon[id] = currentPoke;
+
+    console.log("Button Clicked pokeSaveButton");
+    const pokemonContainer = document.querySelector(".pokemon-container");
+    const pokemonEl = document.createElement("div");
+    pokemonEl.classList.add("pokemon");
+    pokemonEl.innerHTML = `
+    <div class="block">
+      <img onclick="showPoke(${currentPoke["id"]})" id="poke${currentPoke["id"]}"src=${currentPoke["sprites"]["front_default"]}>${currentPoke.name}
+    </div>
+    `;
+    pokemonContainer.appendChild(pokemonEl);
+  }
+});
+
+
+function toggleImage() {
+  if (flag == false) {
+    console.log("pokeCardImage image matched");
+    $("#pokemonImage").attr("src", currentPoke["sprites"]["front_default"] || "");
+    flag = true;
+  } else {
+    console.log("pokeCardImage image not matched");
+    $("#pokemonImage").attr("src", currentPoke["sprites"]["back_default"] || "");
+    flag = false;
+  }
+}
+
+
+function showPoke(id) {
+  console.log("Show poke" + id);
+  getPokemonGeneralInfo(cachedPokemon[id]);
+}
